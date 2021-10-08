867070d367c2e132df00b2d1bfa643ab5543a347
jaboto <jaime@jotabete.com>
2021-10-08 11:20:16 +0200

    Added Program Rules condition verification

--- metadata-health.py
+++ metadata-health.py
@@ -3,7 +3,6 @@ import argparse
 import json
 import pprint
  
-
 # Some constants
 being_verbose = False
 dry_run = False
@@ -37,10 +36,63 @@ def be_verbose(message):
     if being_verbose:
         print(message)
 
+def program_rules_syntax(dry=True, fix=False):
+    # Get all the programs
+    # Check all the PR per program
+    metadata = api_caller_get('programs.json?fields=id')
+    programs = metadata['programs']
+    for program in programs:
+        # Check condition
+        metadata = api_caller_get('programRules.json?fields=id,condition&filter=program.id:eq:{}'.format(program['id']))
+        programRules = metadata['programRules']
+        for programRule in programRules:
+            payload = programRule['condition']
+            response = api_caller_post('programRules/condition/description?programId={}'.format(program['id']), payload)
+            # API response seems to be
+            #   status OK -> Valid expression
+            #   status ERROR -> Invalid expression
+            if response['status'] == 'OK':
+                # Expression is valid
+                be_verbose("ProgramRule condition {} seems to be valid".format(payload))
+            else:
+                print ("ERROR: The following ProgramRule condition is wrong. Please verify!")
+                print ("  Condition: {} ".format(payload))
+                print ("  Hint: {}".format(response['description']))
+                print ("  Please fix the program rule condition: {}/dhis-web-maintenance/index.html#/edit/programSection/programRule/{}".format(url,programRule['id']))
+
+    # Check all the ProgramRuleActions (this works on version >2.37)
+    #   some of them might contain data field, which indicates an expression that requires validation
+    #    if they don't , there is no need to validate (for example they might be hidding only a field)
+    metadata = api_caller_get('system/info')
+    version = metadata['version'].split(".")[1].split("-")[0] # if version comes like 2.38-embargo
+    if int(version) > 37:
+        be_verbose("DHIS2 Version > 2.37: Performing ProgramRuleAction checks")
+        metadata = api_caller_get('programRuleActions.json?fields=id,programRule[id,program[id]],data')
+        programRuleActions = metadata['programRuleActions']
+        for programRuleAction in programRuleActions:
+            if "data" in programRuleAction:
+                payload = programRuleAction['data']
+                response = api_caller_post('programRuleActions/data/expression/description?programId={}'.format(metadata['program']['id']), payload)
+                # API response seems to be
+                #   status OK -> Valid expression
+                #   status ERROR -> Invalid expression
+                if response['status'] == 'OK':
+                    # Expression is valid
+                    be_verbose("ProgramRuleAction condition {} seems to be valid".format(payload))
+                else:
+                    print ("ERROR: The following ProgramRuleAction condition is wrong. Please verify!")
+                    print ("  Condition: {} ".format(payload))
+                    print ("  Hint: {}".format(response['description']))
+                    print ("  Please fix the program rule action condition: {}/dhis-web-maintenance/index.html#/edit/programSection/programRule/{}".format(url,metadata['program']['id']))
+    else:
+        be_verbose("DHIS2 Version < 2.37. ProgramRuleActions check is not supported")
+
+
+
 def duplicated_dataElements_in_sections(dry=True, fix=False):
     # List all the sections and list if any
     #  has duplicated data elements inside
-    metadata = api_caller('sections.json?fields=id,dataElements')
+    metadata = api_caller_get('sections.json?fields=id,dataElements')
     sections = metadata['sections']
     sections_with_duplicated_dataelements = []
     for section in sections:
@@ -74,7 +126,7 @@ def duplicated_dataElements_in_sections(dry=True, fix=False):
 def duplicated_dataElements_in_dataSets(dry=True, fix=False):
     # List all the datasets and list if any
     #  has duplicated data elements inside
-    metadata = api_caller('dataSets.json?fields=id,dataSetElements')
+    metadata = api_caller_get('dataSets.json?fields=id,dataSetElements')
     dataSets = metadata['dataSets']
     datasets_with_duplicated_dataelements = []
     for dataSet in dataSets:
@@ -111,7 +163,7 @@ def duplicated_categoryOptionCombos_in_categoryCombos(dry=True, fix=False):
 def duplicated_categoryOptions_in_categories(dry=True, fix=False):
     # Search all categories and list if any has
     #  duplicated category options
-    metadata = api_caller('categories.json?fields=id,categoryOptions')
+    metadata = api_caller_get('categories.json?fields=id,categoryOptions')
     categories = metadata['categories']
     duplicated_categories = []
     for category in categories:
@@ -164,7 +216,7 @@ def duplicated_UID(dry=True, fix=False):
     #       Duplicated UIDs in all the metadata from the server
     # Available Fix:
     #       Not available, the user should manually modify the UID 
-    metadata = api_caller('metadata.json?fields=id')
+    metadata = api_caller_get('metadata.json?fields=id')
     if metadata == False:
         print ("Cannot check for duplicated_UID. Server responded badly!")
         return False
@@ -199,12 +251,37 @@ def duplicated_UID(dry=True, fix=False):
 
     pprint.pprint(json)
 
-def api_caller(api_call):
+def api_caller_post(api_call, payload):
+    # Perform an API POST call and return a dict with the content
+    api_request = '{}/{}&paging=False'.format(api_url, api_call)
+    be_verbose("Querying the API: "+api_request+" with payload: "+payload)
+
+    try:
+        r = requests.post(api_request, auth=(usr, pwd), data=payload)
+        if r.status_code == 200:
+            return json.loads(r.content)
+        elif r.status_code == 404:
+            print("Error 404")
+            return False
+        else:
+            print("Error UNKNOWN")
+            return False
+    except Exception as e:
+        error_msg = "There was a problem with the API call: \n{}\n  \
+                    Please verify the error:\n{}".format(api_request, e)
+        print(error_msg)
+        exit(1)
+
+def api_caller_get(api_call):
     # Perfrom an API call and return a dict with the content for valid requests
-    # or false whenever there is an error with the reuqest (code != 200)
+    # or false whenever there is an error with the request (code != 200)
 
     # Always make paging = False
-    api_request = '{}/{}&paging=False'.format(api_url, api_call)
+    if "?" in api_call:
+        api_request = '{}/{}&paging=False'.format(api_url, api_call)
+    else:
+        api_request = '{}/{}?paging=False'.format(api_url, api_call)
+
     be_verbose("Querying the API: "+api_request)
 
     try:
@@ -231,18 +308,21 @@ def main():
     global usr
     global pwd
     global api_url
+    global url
     args = parse_arguments()
     if args.verbose == True:
         being_verbose = True
     if args.dry_run == True:
         dry_run = True
+    url = args.server_url
     api_url = args.server_url+'/api'
     usr = args.username
     pwd = args.password
-    duplicated_UID(True, False)
-    duplicated_elements_in_all(True, False)
-    duplicated_dataElements_in_dataSets(True, False)
-    duplicated_dataElements_in_sections(True, False)
+    #duplicated_UID(True, False)
+    #duplicated_elements_in_all(True, False)
+    #duplicated_dataElements_in_dataSets(True, False)
+    #duplicated_dataElements_in_sections(True, False)
+    program_rules_syntax(True, False)
  
  
 if __name__ == "__main__":
